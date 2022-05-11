32e036254a058a95d54e4b89c15f12adc456628e
Ian Whalen <ian.whalen@quantumblack.com>
2022-05-11 08:33:25 +0100

    Update version to use kedro viz.

--- kedro/__init__.py
+++ kedro/__init__.py
@@ -3,7 +3,7 @@ data pipelines by providing uniform project templates, data abstraction,
 configuration and pipeline assembly.
 """
 
-__version__ = "0.18.0kino"
+__version__ = "9001.0.0"
 
 
 import logging
